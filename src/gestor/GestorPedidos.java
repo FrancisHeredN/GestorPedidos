ba623969cedb997ba6611fce7544073b046a8ede
fjheredia75 <116654492+fjheredia75@users.noreply.github.com>
2026-06-23 18:30:24 -0600

    "Aplicacion de DIP y separacion de persistencia"

---
+++ src/gestor/GestorPedidos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestor;
+
+import repositorio.PedidoRepository;
+
+/**
+ *
+ * @author PC
+ */
+public class GestorPedidos {
+
+    PedidoRepository repository,
+    FacturaService facturaService,
+}
+{
+    this.repository = repository;
+    this.facturaService = facturaService;
+
+}
